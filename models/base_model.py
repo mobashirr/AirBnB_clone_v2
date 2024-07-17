4e1e175414f9103edd6dad22f755c5df4ff9d6df
sozoia <zfzg2232@gmail.com>
2024-07-17 23:52:26 +0300

    handle envriment vars and updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 from datetime import datetime
--- models/city.py
+++ models/city.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Defines the City class."""
-from sqlalchemy import Column, String, ForeignKey
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 class City(BaseModel, Base):
-    """Represents a city for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-    Inherits from BaseModel for common attributes.
-    """
+    """ City class """
     __tablename__ = 'cities'
-    
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
