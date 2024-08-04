93ba84e45efb21a51203bbbbcadc94559dfc70a5
sozoia <zfzg2232@gmail.com>
2024-08-04 22:57:20 +0300

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,7 +26,7 @@ printf %s "server {
     root   /var/www/html;
     index  index.html index.htm;
 
-    location /hbnb_static {
+    location /hbnb_static/ {
         alias /data/web_static/current;
         index index.html index.htm;
     }
